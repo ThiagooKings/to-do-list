a205618c43fdab66e202000796b33b3510cb1cf9
Thiago Reis <thiagomreeis@gmail.com>
2024-06-09 21:22:51 -0300

    chore: Add bcrypt password in new users

--- api/src/modules/users/repositories/useCase/createUser/CreateUserUseCase.ts
+++ api/src/modules/users/repositories/useCase/createUser/CreateUserUseCase.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from "tsyringe";
 import { ICreateUsersDTO } from "../../../dtos/ICreateUsersDTO";
 import { IUsersRepository } from "../../IUsersRepository";
+import { hash } from "bcrypt";
 
 @injectable()
 class CreateUserUseCase {
@@ -17,10 +18,12 @@ class CreateUserUseCase {
       throw new Error ("User already exists!");
     }
 
+    const passwordHash = await hash(password, 8);
+
     const user = await this.userRepository.create({
       name,
       email,
-      password
+      password: passwordHash
     });
 
     return user;
