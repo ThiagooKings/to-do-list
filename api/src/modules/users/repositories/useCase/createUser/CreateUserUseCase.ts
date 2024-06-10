e2d5bac4fd23f8af5299d71e67330bb5bb05ced3
Thiago Reis <thiagomreeis@gmail.com>
2024-06-10 08:13:24 -0300

    feat: Creating error message system

--- api/src/modules/users/repositories/useCase/createUser/CreateUserUseCase.ts
+++ api/src/modules/users/repositories/useCase/createUser/CreateUserUseCase.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from "tsyringe";
 import { ICreateUsersDTO } from "../../../dtos/ICreateUsersDTO";
 import { IUsersRepository } from "../../IUsersRepository";
 import { hash } from "bcrypt";
+import { AppError } from "../../../../../errors/AppError";
 
 @injectable()
 class CreateUserUseCase {
@@ -15,7 +16,7 @@ class CreateUserUseCase {
     const userAlreadyExists = await this.userRepository.findByEmail(email);
 
     if (userAlreadyExists) {
-      throw new Error ("User already exists!");
+      throw new AppError("User already exists!");
     }
 
     const passwordHash = await hash(password, 8);
