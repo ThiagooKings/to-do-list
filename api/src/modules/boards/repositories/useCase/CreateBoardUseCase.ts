cd7b0f74857bffb3f0d53cb73ac73187a9e9cb63
Thiago Reis <thiagomreeis@gmail.com>
2024-06-13 20:53:50 -0300

    fix: Change the location of Error in create a new board

--- api/src/modules/boards/repositories/useCase/CreateBoardUseCase.ts
+++ api/src/modules/boards/repositories/useCase/CreateBoardUseCase.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from "tsyringe";
 import { IBoardsRepository } from "../IBoardsRepository";
 import { ICreateBoardsDTO } from "../../dtos/ICreateBoardsDTO";
 import { Board } from "@prisma/client";
+import { AppError } from "../../../../errors/AppError";
 
 @injectable()
 class CreateBoardUseCase {
@@ -11,6 +12,12 @@ class CreateBoardUseCase {
   ){ }
 
   async execute ({ name, status, usersOnBoard }: ICreateBoardsDTO):Promise<Board>{
+
+    if (!usersOnBoard || (usersOnBoard && usersOnBoard.length === 0) ||
+       (!usersOnBoard.find(user => (user.isAdmin)))) {
+
+       throw new AppError("You must include at least one admin user when creating a new board!");
+    }
 
     const board = await this.boardRepository.create({ name, status, usersOnBoard });
 
